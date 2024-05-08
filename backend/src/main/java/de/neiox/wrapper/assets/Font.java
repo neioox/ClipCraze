8a8731014d943f774422aa347d42e6f1b8f79f25
neiox <83120909+neioox@users.noreply.github.com>
2024-05-08 15:51:53 +0200

    added ffmpegwrapper bc of performace stuff

---
+++ backend/src/main/java/de/neiox/wrapper/assets/Font.java
@@ -0,0 +1,21 @@
+package de.neiox.wrapper.assets;
+
+public class Font {
+
+    public String name = "";
+
+    public int size = 0;
+
+
+    public String  color = "";
+    public int alignment = 0;
+
+
+    public Font(String name, int size, String color, int alignment) {
+        this.name = name;
+        this.size = size;
+        this.color = color;
+        this.alignment = alignment;
+    }
+
+}
